b8070a29bc63d49f98456e3df47ff60510e867a5
Andre Burke <aburke.dev@gmail.com>
2022-11-19 15:54:55 -0700

    added more fleshed out stack class

--- src/array.ts
+++ src/array.ts
@@ -5,15 +5,6 @@
 // reverse3([5, 11, 9]) → [9, 11, 5]
 // reverse3([7, 0, 0]) → [0, 0, 7]
 
-/*
-* looping through the array - includes incrementing 
-* for loop {
-    what goes here?
-
-    reassign the value of the array - more detail
-    TODO: expand this to work for an array of size n
-  }
-*/
 export const reverse3 = (array: number[]): number[] => {
   let firstNum: number = array[0]; // 3
   let lastNum: number = array[array.length - 1]; // 7
@@ -22,34 +13,38 @@ export const reverse3 = (array: number[]): number[] => {
   return array;
 };
 
-//
-// [5, 2, 3, 4, 1], [2, 4, 6, 8, 10, 12, 14, 16]
-//    i^     ^j
-// 1 array.len / 2 (middle)
-// 2 something that captures just one index in the array
-// 3 replace the original 1 with the 5, array[i] = array[j], create a varaible to capture the end of the array
-// 4 array[j] = temp;
-class Stack<T> {
-  private stack: T[] = [];
-  private size: number = 0;
+export class Stack<T> {
+  private stack: T[];
+  private size: number;
 
-  constructor() {}
+  constructor() {
+    this.stack = [];
+    this.size = 0;
+  }
 
-  // [10] size = 1
   push(item: T): void {
     this.stack[this.size++] = item;
   }
 
-  pop(): T | null {
-    if (this.size === 0) {
-      return null;
-    }
+  pop(): T {
+    if (this.isEmpty()) throw new Error('Cannot pop from an empty stack.');
 
-    this.size--; // 0
+    this.size = this.size - 1;
     return this.stack[this.size];
+  }
+
+  peek(): T {
+    if (this.isEmpty()) throw new Error('Cannot pop from an empty stack.');
+
+    return this.stack[this.size - 1];
+  }
+
+  isEmpty(): boolean {
+    return this.size === 0;
   }
 }
 
+// recursive approach
 export const reverse = (str: string): string => {
   if (str === '') return '';
 
@@ -58,20 +53,25 @@ export const reverse = (str: string): string => {
   return char + reverse(str.substring(0, end));
 };
 
-export const improvedReverse = (array: number[]): number[] => {
-  // [1, 2, 3, 4, 5]
+// [5, 2, 3, 4, 1], [2, 4, 6, 8, 10, 12, 14, 16]
+//    i^     ^j
+// 1 array.len / 2 (middle)
+// 2 something that captures just one index in the array
+// 3 replace the original 1 with the 5, array[i] = array[j], create a varaible to capture the end of the array
+// 4 array[j] = temp;
+export const improvedReverse = (nums: number[]): number[] => {
+  // auxillary data structure approach
   const stack: Stack<number> = new Stack();
 
-  // array.reverse();
-
-  array.forEach((element) => {
+  nums.forEach((element) => {
     stack.push(element);
   });
 
-  for (let index = 0; index < array.length; index++) {
-    array[index] = stack.pop() as number;
+  for (let index = 0; index < nums.length; index++) {
+    nums[index] = stack.pop();
   }
 
+  // iterative approach
   // let n: number = array.length / 2;
   // let j: number = array.length - 1;
 
@@ -81,5 +81,5 @@ export const improvedReverse = (array: number[]): number[] => {
   //   array[j] = temp;
   // }
 
-  return array;
+  return nums;
 };
